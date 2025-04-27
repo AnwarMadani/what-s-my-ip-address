dc158a23fb8399baf21cef4d1a6b82d0950591a5
Anwar Madani <15781928+AnwarMadani@users.noreply.github.com>
2025-04-27 10:21:43 +0200

    add basic layout and script

--- README.md
+++
@@ -1,1 +0,0 @@
-# what-s-my-ip-adress
---
+++ script.js
@@ -0,0 +1,13 @@
+function lookupIP(ip = '') {
+    const url = ip ? `https://ipwho.is/${ip}` : `https://ipwho.is/`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        if(!data.success){
+            alert('Invalid IP address!');
+            return;
+        }
+        console.log(data)
+    })
+}
